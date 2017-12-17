3af6c249966a952cab84b15918491b7acf6ba11c
avmityakov <avmityakov@gmail.com>
2017-12-17 18:54:43 +0300

    new active router

--- src/renderer/components/NavigationMenu/NavigationButton.vue
+++ src/renderer/components/NavigationMenu/NavigationButton.vue
@@ -21,11 +21,14 @@
 </script>
 
 <style lang="scss" scoped>
+    $height: 40px;
+    $angle: 10px;
+
     .navigation-button {
         color: white;
         text-decoration: none;
         width: 100%;
-        height: 40px;
+        height: $height;
         text-align: center;
         display: flex;
         justify-content: center;
@@ -35,13 +38,26 @@
         background-image: linear-gradient(#3a3f4b, #353b45);
         border-radius: 5px;
         box-sizing: border-box;
+        position: relative;
 
         &:hover {
             background-image: linear-gradient(#3e4451, #3a3f4b);
         }
 
-        &.router-link-active {
-            border-left: 4px solid $active-color;
+        &.router-link-active:after {
+            content: "";
+            position: absolute;
+            bottom: 0;
+            width: 0;
+            height: 0;
+            border-top: $height / 2 solid transparent;
+            border-bottom: $height / 2 solid transparent;
         }
+
+        &.router-link-active:after {
+            left: 0;
+            border-left: $angle solid $active-color;
+        }
+
     }
 </style>
